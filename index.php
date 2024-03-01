f1086e67958b5686ac019a94c02cc8f11b72f927
AndreaParrini <andrea.parrini26@gmail.com>
2024-03-01 20:53:42 +0100

    Add snack 1
    stamp in html all results

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
+<?php
+/* 
+
+## Snack 1
+
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60
+
+*/
+
+$matches = [
+    [
+        'teamAway' => 'Team Away 1',
+        'teamHome' => 'Team Home 1',
+        'points_teamAway' => 80,
+        'points_teamHome' => 88,
+    ],
+    [
+        'teamAway' => 'Team Away 2',
+        'teamHome' => 'Team Home 2',
+        'points_teamAway' => 65,
+        'points_teamHome' => 92,
+    ],
+    [
+        'teamAway' => 'Team Away 3',
+        'teamHome' => 'Team Home 3',
+        'points_teamAway' => 78,
+        'points_teamHome' => 62,
+    ],
+    [
+        'teamAway' => 'Team Away 4',
+        'teamHome' => 'Team Home 4',
+        'points_teamAway' => 66,
+        'points_teamHome' => 64,
+    ],
+    [
+        'teamAway' => 'Team Away 5',
+        'teamHome' => 'Team Home 5',
+        'points_teamAway' => 63,
+        'points_teamHome' => 79,
+    ]
+]
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +52,17 @@
     <title>Snacks B1</title>
 </head>
 <body>
-    
+    <!-- Snack 1 -->
+    <?php foreach ($matches as $match) : ?>
+        <div>
+            <span><?php echo $match['teamAway']?></span> -
+            <span><?php echo $match['teamHome']?></span> | 
+            <span><?php echo $match['points_teamAway']?></span> -
+            <span><?php echo $match['points_teamHome']?></span>
+        </div> 
+    <?php endforeach; ?>
+    <!-- end Snack 1 -->
+
+
 </body>
 </html>
